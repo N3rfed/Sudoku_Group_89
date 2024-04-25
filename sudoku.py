f32af78dd6dfea90d69c035b0725842e59e364de
jasonhe <jhe2@ufl.edu>
2024-04-25 13:23:14 -0400

    got gud

--- sudoku.py
+++ sudoku.py
@@ -6,6 +6,50 @@ LINE_COLOR = (30, 227, 49)
 HEIGHT = 1000
 WIDTH = 1000
 
+def draw_game_start(screen):
+    start_title_font = pygame.font.Font(None,100)
+    button_font = pygame.font.Font(None, 70)
+
+    screen.fill((255,255,255))
+
+    title_surface = start_title_font.render("Sudoku", 0 , (LINE_COLOR))
+    title_rectangle = title_surface.get_rect(center=(WIDTH // 2, HEIGHT // 2 - 150))
+    screen.blit(title_surface, title_rectangle)
+
+    easy_text = button_font.render("Easy", 0, (255,255,255))
+    medium_text = button_font.render("Medium", 0, (255,255,255))
+    hard_text = button_font.render("Hard", 0, (255,255,255))
+
+    easy_surface = pygame.Surface((easy_text.get_size()[0] +20, easy_text.get_size()[1] +20))
+    easy_surface.fill(LINE_COLOR)
+    easy_surface.blit(easy_text,(10,10))
+    medium_surface = pygame.Surface((medium_text.get_size()[0] +20, medium_text.get_size()[1] +20))
+    medium_surface.fill(LINE_COLOR)
+    medium_surface.blit(medium_text, (10, 10))
+    hard_surface = pygame.Surface((hard_text.get_size()[0] + 20, hard_text.get_size()[1] + 20))
+    hard_surface.fill(LINE_COLOR)
+    hard_surface.blit(hard_text, (10, 10))
+
+    easy_rectangle = easy_surface.get_rect(center =(WIDTH//2, HEIGHT//2 + 50))
+    medium_rectangle = medium_surface.get_rect(center =(WIDTH//2, HEIGHT//2 + 150))
+    hard_rectangle = hard_surface.get_rect(center=(WIDTH//2, HEIGHT//2 + 250))
+
+    screen.blit(easy_surface,easy_rectangle)
+    screen.blit(medium_surface,medium_rectangle)
+    screen.blit(hard_surface, hard_rectangle)
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if easy_rectangle.collidepoint(event.pos):
+                    return 30
+                if medium_rectangle.collidepoint(event.pos):
+                    return 40
+                if hard_rectangle.collidepoint(event.pos):
+                    return 50
+        pygame.display.update()
 def won_game(screen):
     win_font = pygame.font.Font(None, 100)
     button_font = pygame.font.Font(None, 70)
@@ -69,54 +113,6 @@ def lost_game(screen):
                     return True
 
         pygame.display.update()
-
-def draw_game_start(screen):
-    start_title_font = pygame.font.Font(None,100)
-    button_font = pygame.font.Font(None, 70)
-
-    screen.fill((255,255,255))
-
-    title_surface = start_title_font.render("Sudoku", 0 , (LINE_COLOR))
-    title_rectangle = title_surface.get_rect(center=(WIDTH // 2, HEIGHT // 2 - 150))
-    screen.blit(title_surface, title_rectangle)
-
-    easy_text = button_font.render("Easy", 0, (255,255,255))
-    medium_text = button_font.render("Medium", 0, (255,255,255))
-    hard_text = button_font.render("Hard", 0, (255,255,255))
-
-    easy_surface = pygame.Surface((easy_text.get_size()[0] +20, easy_text.get_size()[1] +20))
-    easy_surface.fill(LINE_COLOR)
-    easy_surface.blit(easy_text,(10,10))
-    medium_surface = pygame.Surface((medium_text.get_size()[0] +20, medium_text.get_size()[1] +20))
-    medium_surface.fill(LINE_COLOR)
-    medium_surface.blit(medium_text, (10, 10))
-    hard_surface = pygame.Surface((hard_text.get_size()[0] + 20, hard_text.get_size()[1] + 20))
-    hard_surface.fill(LINE_COLOR)
-    hard_surface.blit(hard_text, (10, 10))
-
-    easy_rectangle = easy_surface.get_rect(center =(WIDTH//2, HEIGHT//2 + 50))
-    medium_rectangle = medium_surface.get_rect(center =(WIDTH//2, HEIGHT//2 + 150))
-    hard_rectangle = hard_surface.get_rect(center=(WIDTH//2, HEIGHT//2 + 250))
-
-    screen.blit(easy_surface,easy_rectangle)
-    screen.blit(medium_surface,medium_rectangle)
-    screen.blit(hard_surface, hard_rectangle)
-
-    while True:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                sys.exit()
-            if event.type == pygame.MOUSEBUTTONDOWN:
-                if easy_rectangle.collidepoint(event.pos):
-                    diff = 30
-                    return diff
-                if medium_rectangle.collidepoint(event.pos):
-                    diff = 40
-                    return diff
-                if hard_rectangle.collidepoint(event.pos):
-                    diff = 50
-                    return diff
-        pygame.display.update()
 def draw_buttons(buttonList):
     button_font = pygame.font.Font(None, 70)
 
@@ -149,9 +145,6 @@ def draw_buttons(buttonList):
     buttonList.append(exit_rectangle)
 
     return
-
-
-
 
 if __name__ == "__main__":
     game_over = False
@@ -216,7 +209,6 @@ if __name__ == "__main__":
                 if game_board.check_board():
                     restart = won_game(screen)
                 else:
-
                     restart = lost_game(screen)
             pygame.display.update()
 
